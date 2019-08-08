ab7a1ed859d8481babe0b9592ae67197419272ba
Mada Gower <gmgower@gmail.com>
2019-08-08 18:45:30 -0500

    Add nested Routes.

--- src/components/AvengerPage.jsx
+++ src/components/AvengerPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import avengers from '../Data.js';
+// import avengers from '../Data.js';
+import { Route, Linke } from 'react-router-dom';
 
 
 
@@ -14,8 +15,13 @@ const AvengerPage = (props) => {
       <div className='character-info-wrapper'>
         <h1>{avenger.name}</h1>
         <h4>({avenger.nickname})</h4>
-        <p>{avenger.description}</p>
+        {/* <p>{avenger.description}</p> */}
       </div> 
+      {/* Nested Routes */}
+     
+      <Route path="/avengers/:id/details" render={props => <p>{avenger.description}</p>}/>
+      <Route path="/avengers/:id/movies" render={props => <div>Movies List</div>} />
+
     </div>
   );
 }
